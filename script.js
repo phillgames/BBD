3d6af63aeb41ab2ab71a285d843c4e13b0084927
phillsearle <102220888+phillgames@users.noreply.github.com>
2024-03-19 11:29:21 +0100

    hover meny
    endra slik at menyen kommer ned ved bare å ha musa over istedet for å klikke på pilen

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-let menuDown = false;
 
 
-function menuToggle() {
 
-    if (menuDown === false) {
+function menuDown() {
+    const rect = document.getElementById("navbar").getBoundingClientRect();
+    if (rect.bottom == 55) {
+
         let arrowdown = document.querySelector(".collection")
         let arrowturn = document.querySelector(".arrow")
         arrowdown.classList.remove("dropuparrow")
         arrowturn.classList.remove("arrowflipback")
         arrowdown.classList.add("dropdownarrow")
         arrowturn.classList.add("arrowflip")
-        menuDown = true;
     }
 
-    else {
+}
+
+function menuUp() {
+    const rect = document.getElementById("navbar").getBoundingClientRect();
+    if (rect.bottom == 145) {
         let arrowdown = document.querySelector(".collection")
         let arrowturn = document.querySelector(".arrow")
         arrowdown.classList.remove("dropdownarrow")
         arrowturn.classList.remove("arrowflip")
         arrowdown.classList.add("dropuparrow")
         arrowturn.classList.add("arrowflipback")
-        menuDown = false;
     }
 }
