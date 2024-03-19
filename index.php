f62ed2ebe084bbd62f59ec4c96bfc54aa605a524
phillsearle <102220888+phillgames@users.noreply.github.com>
2024-03-19 12:52:46 +0100

    fuck off

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:index.html
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,45 +37,4 @@
     <script src="script.js"></script>
 </body>
 
-=======
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blackbox-Development</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://use.typekit.net/kjp3glf.css">
-    <link rel="icon" type="images/x-icon" href="Imgs/Logo-BBD.svg" />
-
-</head>
-
-<body>
-    <div class="collection">
-        <div class="menunav">
-            <h1 id="topleft" class="topbuttons">
-                what we offer
-            </h1>
-            <h1 id="topmid" class="topbuttons">
-                Our consultants
-            </h1>
-            <h1 id="topright" class="topbuttons">
-                about us
-            </h1>
-            <a href="login.php" id="topright" class="topbuttons">
-                Log in / Signup
-            </a>
-        </div>
-        <div class="dropdown" onclick="menuToggle()">
-            <img  class="arrow" src="Imgs/Dropdown-arrow.svg" alt="">
-        </div>
-    </div>
-    <div class="maintext">
-        <img src="Imgs/front-text.svg" alt="">
-    </div>
-    <script src="script.js"></script>
-</body>
-
->>>>>>> d40b394a1cc35e36a7506f4d7ddf99b5e11b313e:index.php
 </html>
